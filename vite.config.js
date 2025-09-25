4f40d600267f293d7a84db042061cd34e167bcaa
santhosh-bs22 <santhosh220506@gmail.com>
2025-09-26 00:16:47 +0530

    Movie Seacrh app

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/movie-search-react/',  // 👈 use your repo name here
+  base: 'samplemoviesearch',  // 👈 use your repo name here
 })
